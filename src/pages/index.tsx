fea626b48bcb02f0f1ca10789588dfb6cb69164b
Willian Soares Damasceno <wilhaods@gmail.com>
2022-03-29 17:12:27 -0300

    Finalização da página Home
    Criação da página home, com inclusão de estilização.
    
    * Configuração do Call to Action
    * Configuação dos apoiadores

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,24 @@ export default function Home() {
         <title>Board - Organizando Tarefas!!!!</title>
       </Head>
     
-      <div>
-        <h1>Brasil acima de tudo. Deus acima de todos!</h1>
-      </div>
+      <main className={styles.contentContainer}>
+        <img src="/images/board-user.svg" alt="Projeto Board" />
+      
+
+        <section className={styles.callToAction}>
+          <h1>
+            Uma ferramenta para seu dia a dia. Escreva, planeje e organize-se...
+          </h1>
+          <p>
+            <span>100% gratuíto</span> e online!
+          </p>
+        </section>
+
+        <div className={styles.donaters}>
+          <img src='https://sujeitoprogramador.com/steve.png' alt="Apoiador 1" />
+         
+        </div>
+      </main>
     </>
   )
 }
