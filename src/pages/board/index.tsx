bc67b2dee1602823c4be075337f9b0a088b27a2b
Willian Soares Damasceno <wilhaods@gmail.com>
2022-04-04 15:16:16 -0300

    Estilo - Board
    Finalização de estilos da página Boards

---
+++ src/pages/board/index.tsx
@@ -0,0 +1,60 @@
+import  Head  from 'next/head';
+import styles from './styles.module.scss';
+import { FiCalendar, FiTrash, FiEdit2, FiPlus, FiClock } from 'react-icons/fi';
+
+export default function Board(){
+  return(
+    <>
+      <Head>
+        <title>Minhas tarefas - Board</title>
+      </Head>
+      <main className={styles.container}>
+        <form>
+          <input 
+            type="text" 
+            placeholder="Digite sua tarefa..."
+          />
+          <button type="submit">
+            <FiPlus size={25} color="#1718if"/>
+          </button>
+        </form>
+
+        <h1>Você tem 2 tarefas</h1>
+
+        <section>
+          <article className={styles.taskList}>
+            <p>Praticar programação por no mínimo 2(duas) horas</p>
+            <div className={styles.actions}>
+              <div>
+                <div>
+                  <FiCalendar size={20} color="#FFB800" />
+                  <time>29 de março de 2022</time>
+                </div>
+                <button>
+                  <FiEdit2 size={0} color="#FFf"/>
+                  <span>Editar</span>
+                </button>
+              </div>
+              
+              <button>
+                <FiTrash size={20} color="#FF3636" />
+                <span>Excluir</span>
+              </button>
+            </div>
+          </article>
+        </section>
+
+      </main>
+
+      <div className={styles.vipContainer}>
+        <h3>Obrigado por apoiar esse projeto.</h3>
+        <div>
+          <FiClock size={28} color="#FFF" />
+          <time>
+            Última doação foi a 3 dias.
+          </time>
+        </div>
+      </div>
+    </>
+  );
+}
